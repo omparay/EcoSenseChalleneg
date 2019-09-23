d3839f6e17960cb7fb92c316769206571e601364
Oliver Paray <oparay@sparkdigital.com>
2019-09-23 11:20:52 -0700

    Initial Commit

---
+++ Calc/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Calc
+//
+//  Created by Oliver Paray on 9/23/19.
+//  Copyright © 2019 Oliver Paray. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
